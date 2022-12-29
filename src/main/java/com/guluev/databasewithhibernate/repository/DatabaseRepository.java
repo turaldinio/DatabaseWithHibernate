d34a9d2ea6f50f64a2d7ea5d86a6a07ba2ccf700
Tural Guluev <tural-98@bk.ru>
2022-12-29 15:16:51 +0300

    added the main body of the DatabaseRepository class

---
+++ src/main/java/com/guluev/databasewithhibernate/repository/DatabaseRepository.java
@@ -0,0 +1,32 @@
+package com.guluev.databasewithhibernate.repository;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Repository;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
+
+@Repository
+public class DatabaseRepository {
+    private String select;
+
+
+    public DatabaseRepository() {
+        select = read("select.sql");
+    }
+
+    private static String read(String scriptFileName) {
+        try (InputStream is = new ClassPathResource(scriptFileName).getInputStream();
+             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is))) {
+            return bufferedReader.lines().collect(Collectors.joining("\n"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String getProductName(String name) {
+
+    }
