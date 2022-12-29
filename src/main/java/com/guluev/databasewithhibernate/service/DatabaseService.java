72a42fff49b3bc24d7e43017d1a61c718f8f43fc
Tural Guluev <tural-98@bk.ru>
2022-12-29 15:24:43 +0300

    the service classs finished

--- src/main/java/com/guluev/databasewithhibernate/service/DatabaseService.java
+++ src/main/java/com/guluev/databasewithhibernate/service/DatabaseService.java
@@ -1,12 +1,18 @@
 package com.guluev.databasewithhibernate.service;
 
+import com.guluev.databasewithhibernate.repository.DatabaseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DatabaseService {
+    private final DatabaseRepository databaseRepository;
 
+    public DatabaseService(DatabaseRepository databaseRepository) {
+        this.databaseRepository = databaseRepository;
+    }
 
     public String getProductName(String name) {
-        return null;
+        return databaseRepository.getProductName(name);
     }
 }
