74a7a51502285a58c3990f573a1679903228b583
Tural Guluev <tural-98@bk.ru>
2022-12-29 22:24:53 +0300

    code clean up

--- src/main/java/com/guluev/databasewithhibernate/model/Orders.java
+++ src/main/java/com/guluev/databasewithhibernate/model/Orders.java
@@ -2,10 +2,7 @@ package com.guluev.databasewithhibernate.model;
 
 import jakarta.persistence.*;
 import lombok.Getter;
-import org.hibernate.annotations.CreationTimestamp;
 
-import java.time.LocalDate;
-import java.util.Collection;
 import java.util.Date;
 
 @Entity
@@ -15,7 +12,7 @@ public class Orders {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-@Column(columnDefinition = "timestamp default current_timestamp")
+    @Column(columnDefinition = "timestamp default current_timestamp")
     private Date date;
 
     @ManyToOne(optional = false)
